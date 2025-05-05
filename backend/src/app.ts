1fb0e9935937f2a14dd4faec88df4a1acb1ac074
Sharathxct <sharathxct@gmail.com>
2025-05-06 01:54:57 +0530

    addition of backend and website

---
+++ backend/src/app.ts
@@ -0,0 +1,23 @@
+import express, { Application, Request, Response, NextFunction } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app: Application = express();
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+app.get('/', (req: Request, res: Response) => {
+  res.send('API Running');
+});
+
+// Error handling middleware
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+export default app;
