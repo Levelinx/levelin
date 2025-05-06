43b08b60d4732d11b80bf08712641c1c946a7ce1
Sharathxct <sharathxct@gmail.com>
2025-05-06 13:33:25 +0530

    privy

--- frontend/providers/tanstack.tsx
+++ frontend/providers/tanstack.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-
+import {PrivyProvider} from '@privy-io/react-auth';
 const OneHoureInMs = 60 * 60 * 1000;
 
 const queryClient = new QueryClient({
@@ -21,8 +21,19 @@ const queryClient = new QueryClient({
 const TanstackProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
     <QueryClientProvider client={queryClient}>
+      <PrivyProvider
+      appId="your-privy-app-id"
+      clientId="your-app-client-id"
+      config={{
+        // Create embedded wallets for users who don't have a wallet
+        embeddedWallets: {
+          createOnLogin: 'users-without-wallets'
+        }
+      }}
+    >
       {children}
-    </QueryClientProvider>
+    </PrivyProvider>
+  </QueryClientProvider>
   );
 };
 
